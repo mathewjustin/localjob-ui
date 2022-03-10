b328185bd0c2191bf3ef460ec2adcb33d7e6d7c5
Justin Mathew <justin@Justins-MacBook-Air.local>
2022-03-10 21:46:55 +0530

    First commit

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -8,19 +8,58 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
+  calllocaljobsapi,
+  selectResult,
+  addValue 
 } from './counterSlice';
 import styles from './Counter.module.css';
+import { setSourceMapRange } from 'typescript';
 
 export function Counter() {
   const count = useAppSelector(selectCount);
+  const result=useAppSelector(selectResult);
+  // const firstValueL=useAppSelector(firstValue);
+  // const secondValueL=useAppSelector(secondValue);
+
+
   const dispatch = useAppDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
 
+   
+  const [valueToAdd, setValueToAdd] = useState({
+    firstValue: 0,
+    secondValue: 0
+    });
+
+    const handleChange = (e: { target: { name: any; value: any; }; }) => {
+      const { name, value } = e.target;
+      setValueToAdd(prevState => ({
+          ...prevState,
+          [name]: value
+      }));
+  };
+    
   const incrementValue = Number(incrementAmount) || 0;
 
   return (
     <div>
       <div className={styles.row}>
+        <input value={valueToAdd.firstValue} name="firstValue" onChange={handleChange}/>
+        <input value={valueToAdd.secondValue} name="secondValue" onChange={handleChange}/>
+        <input value={result} readOnly></input>
+        <button
+          className={styles.button}
+          aria-label="Add Value"
+           onClick={() => dispatch(addValue(valueToAdd))}
+        >Add 2 numberss</button>
+
+<button
+          className={styles.button}
+          aria-label="Add Value API"
+           onClick={() => dispatch(calllocaljobsapi(valueToAdd))}
+        >Add Value API</button>
+
+ 
         <button
           className={styles.button}
           aria-label="Decrement value"
@@ -56,6 +95,11 @@ export function Counter() {
         >
           Add Async
         </button>
+
+        
+          Call custom API
+       
+
         <button
           className={styles.button}
           onClick={() => dispatch(incrementIfOdd(incrementValue))}
@@ -66,3 +110,7 @@ export function Counter() {
     </div>
   );
 }
+
+
+
+
--- src/features/counter/counterSlice.spec.ts
+++ src/features/counter/counterSlice.spec.ts
@@ -9,6 +9,7 @@ describe('counter reducer', () => {
   const initialState: CounterState = {
     value: 3,
     status: 'idle',
+    result:0
   };
   it('should handle initial state', () => {
     expect(counterReducer(undefined, { type: 'unknown' })).toEqual({
